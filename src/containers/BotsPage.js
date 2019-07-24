6bbde84c446cd529738c07932f9b8c2291c46669
John Linatoc <johnlinatoc1227@gmail.com>
2019-07-24 09:20:27 -0500

    initial commit

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -2,8 +2,24 @@ import React from "react";
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  constructor(props){
+    super(props)
+    this.state = {
+      data: []
+    }
+  }
+
+
+  componentDidMount(){
+    const url = "https://bot-battler-api.herokuapp.com/api/v1/bots"
+
+    fetch(url)
+    .then(resp => resp.json())
+    .then(data => this.setState({ data }))
+  }
 
   render() {
+    const bots = this.state.data
     return (
       <div>
         {/* put your components here */}
