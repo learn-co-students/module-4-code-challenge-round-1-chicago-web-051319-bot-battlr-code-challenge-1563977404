97759bde2e1fca45536ecb33f35b021021a7682f
Joel Seng <joel29dec@gmail.com>
2019-07-24 09:23:18 -0500

    checkin

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -3,13 +3,15 @@ import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
   //your code here
-
+	
   render(){
+	const bots = this.props.bots
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+    		  {bots.map(bot => {
+				  return <BotCard bot={bot}/>
+			  })}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,30 @@
 import React from "react";
-
+import YourBotArmy from './YourBotArmy'
+import BotCollection from './BotCollection'
+import { SSL_OP_CISCO_ANYCONNECT } from "constants";
+const API = 'https://bot-battler-api.herokuapp.com/api/v1/bots'
 class BotsPage extends React.Component {
   //start here with your code for step one
+  constructor(props) {
+    super(props);
+    this.state = { 
+      bots: []
+     }
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(resp => resp.json())
+    .then(data => this.setState({
+      bots: data
+    }))
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy/>
+        <BotCollection bots={this.state.bots}/>
       </div>
     );
   }
@@ -14,3 +32,4 @@ class BotsPage extends React.Component {
 }
 
 export default BotsPage;
+
