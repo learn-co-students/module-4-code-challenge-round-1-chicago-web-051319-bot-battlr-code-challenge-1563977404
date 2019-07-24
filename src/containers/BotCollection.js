21721b3e47e0fae1838f532933497cbd3d997b70
xmksz3 <markus.sanderst@gmail.com>
2019-07-24 10:56:09 -0500

    Done

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -10,14 +10,14 @@ class BotCollection extends React.Component {
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-				Collection of all bots <br/>
+				Collection of all bots <br />
 			  {this.props.bots.map(bot => {
-			  return <BotCard
+			  return (<BotCard
 				  key={bot.id}
 				  bot={bot}
 				  addBotToArmy={this.props.addBotToArmy}
 				  botShowPage={this.props.botShowPage}
-				/>
+				/>)
 			  })
 			  }
     		</div>
